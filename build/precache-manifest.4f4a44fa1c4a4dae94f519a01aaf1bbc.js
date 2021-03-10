f6f0307df7797286faf41a57eeadea1bd93b03a0
Luke Roznovsky <lukeroz1@gmail.com>
2021-03-09 20:35:19 -0600

    Added build folder from main project

---
+++ build/precache-manifest.4f4a44fa1c4a4dae94f519a01aaf1bbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "69a7dbe5974ebf41780c",
+    "url": "/social-network/static/css/main.f4a30713.chunk.css"
+  },
+  {
+    "revision": "69a7dbe5974ebf41780c",
+    "url": "/social-network/static/js/main.69a7dbe5.chunk.js"
+  },
+  {
+    "revision": "f27067f9f410f6786daa",
+    "url": "/social-network/static/css/1.c28afed4.chunk.css"
+  },
+  {
+    "revision": "f27067f9f410f6786daa",
+    "url": "/social-network/static/js/1.f27067f9.chunk.js"
+  },
+  {
+    "revision": "22e660ce69f7e2ad833f",
+    "url": "/social-network/static/js/runtime~main.22e660ce.js"
+  },
+  {
+    "revision": "518bfa4fb14c4a5d2b64a4992e87ddee",
+    "url": "/social-network/index.html"
+  }
+];
