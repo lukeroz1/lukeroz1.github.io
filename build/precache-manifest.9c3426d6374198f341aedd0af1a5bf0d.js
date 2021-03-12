21959ffaabeb0ea6a7214cbb8cde0172ac50ac67
Luke Roznovsky <lukeroz1@gmail.com>
2021-03-12 16:06:47 -0600

    Build Five (Ropsten Deployment)

---
+++ build/precache-manifest.9c3426d6374198f341aedd0af1a5bf0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e510d4ef3baea2c655f2",
+    "url": "/build/static/css/main.fbbed5ce.chunk.css"
+  },
+  {
+    "revision": "e510d4ef3baea2c655f2",
+    "url": "/build/static/js/main.e510d4ef.chunk.js"
+  },
+  {
+    "revision": "d889ca8e71b969da12e2",
+    "url": "/build/static/css/1.d176e876.chunk.css"
+  },
+  {
+    "revision": "d889ca8e71b969da12e2",
+    "url": "/build/static/js/1.d889ca8e.chunk.js"
+  },
+  {
+    "revision": "3923d73d01a4271659cc",
+    "url": "/build/static/js/runtime~main.3923d73d.js"
+  },
+  {
+    "revision": "e5233c35df87a99cf55ac87a5c734d27",
+    "url": "/build/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/build/precache-manifest.78754dea8a7b674fc563984ba7212832.js"
+  "/build/precache-manifest.9c3426d6374198f341aedd0af1a5bf0d.js"
 );
 
 workbox.clientsClaim();
